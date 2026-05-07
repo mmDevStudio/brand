6f272fbd63065d84604000f3936948579024c8ad
Mika Reich <mikareich0@gmail.com>
2026-05-07 21:55:04 +0200

    feat(ui): add Input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,3 +1,39 @@
-export default function Input({ ...props }: any) {
-  return <div></div>;
+import { cva } from "class-variance-authority";
+import React from "react";
+import cn from "@/utils/cn";
+
+export const inputContainerStyles = cva(
+  `flex items-center border-border border-1 px-3 py-2 gap-3 bg-bg
+  text-text text-body font-body w-min transition
+  focus-within:bg-bg-accent focus-within:outlined hover:bg-bg-accent`,
+);
+
+export const inputStyles = cva(
+  "outline-0 flex-1 min-w-0 placeholder:text-border",
+);
+
+type InputProps = React.ComponentProps<"input"> & {
+  prefixIcon?: React.ReactNode;
+  suffixIcon?: React.ReactNode;
+};
+
+export default function Input({
+  prefixIcon,
+  suffixIcon,
+  className,
+  id,
+  ...props
+}: InputProps) {
+  const fallbackId = React.useId();
+  id ||= fallbackId;
+
+  return (
+    <label className={cn(inputContainerStyles(), className)} htmlFor={id}>
+      {prefixIcon && <span className="shrink-0">{prefixIcon}</span>}
+
+      <input id={id} className={inputStyles()} {...props} />
+
+      {suffixIcon && <span className="shrink-0">{suffixIcon}</span>}
+    </label>
+  );
 }
