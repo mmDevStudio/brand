e2b3780d406bb0180a336479ded5249872e86cfa
Mika Reich <mikareich0@gmail.com>
2026-08-13 13:32:17 +0000

    chore: upgrade Next.js and TypeScript

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: ["dev-box"],
+  experimental: {
+    turbopackRustReactCompiler: true,
+    useTypeScriptCli: true,
+  },
 };
 
 export default nextConfig;
