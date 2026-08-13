cac5bb860fe179b0ed694eab7e4a040c9a83d1fa
Mika Reich <mikareich0@gmail.com>
2026-08-13 20:17:50 +0000

    refactor: sample scrolling stripes with CSS

--- src/components/AppBar/NavBar.tsx
+++ src/components/AppBar/NavBar.tsx
@@ -1,41 +1,15 @@
-"use client";
-
-import { useEffect, useRef } from "react";
 import { sections } from "@/lib/content";
 import AppBar from "./AppBar";
 
 export default function NavBar() {
-  const ref = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const element = ref.current;
-    if (!element) return;
-
-    const syncBackground = () => {
-      const bodyStyle = getComputedStyle(document.body);
-      const rect = element.getBoundingClientRect();
-      const naturalTop = window.scrollY + rect.top;
-      const backgroundY =
-        window.scrollY >= naturalTop ? -window.scrollY : -naturalTop;
-
-      element.style.backgroundPosition = `${-parseFloat(bodyStyle.paddingLeft) + 0.25}px ${backgroundY}px`;
-    };
-
-    syncBackground();
-    window.addEventListener("scroll", syncBackground, { passive: true });
-    window.addEventListener("resize", syncBackground, { passive: true });
-
-    return () => {
-      window.removeEventListener("scroll", syncBackground);
-      window.removeEventListener("resize", syncBackground);
-    };
-  }, []);
-
   return (
-    <div
-      ref={ref}
-      className="sticky px-px -my-px top-0 z-10 bg-bg bg-stripes border-b border-border"
-    >
+    <div className="sticky isolate px-px -my-px top-0 z-10 border-b border-border">
+      <span
+        aria-hidden
+        className="pointer-events-none absolute inset-0 -z-10 bg-bg [clip-path:inset(0)]"
+      >
+        <span className="fixed inset-0 bg-bg bg-stripes" />
+      </span>
       <span className="pointer-events-none absolute -bottom-px left-1/2 -translate-x-1/2 w-screen border-t border-dashed border-border" />
       <span className="pointer-events-none absolute left-0 bottom-0 translate-y-1/2 h-[200vh] border-l border-dashed border-border" />
       <span className="pointer-events-none absolute right-0 bottom-0 translate-y-1/2 h-[200vh] border-r border-dashed border-border" />
