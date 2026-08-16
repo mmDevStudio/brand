80161018ecb38f55d5c92a4c227da9cad6bea2f4
Mika Reich <mikareich0@gmail.com>
2026-08-16 16:39:11 +0000

    refactor: simplify subcomponent pattern of AppBar

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -1,21 +1,20 @@
 import { Slot } from "@radix-ui/react-slot";
-import Link, { type LinkProps } from "next/link";
-import type { ComponentProps, HTMLAttributes, ReactNode } from "react";
+import Link from "next/link";
+import type { ComponentProps } from "react";
 import cn from "@/lib/cn";
 
-type AppBarProps = HTMLAttributes<HTMLElement> & {
-  as?: "nav" | "footer";
+const itemClassName =
+  "flex items-center text-action cursor-pointer transition select-none bg-transparent text-primary highlight hover-highlight px-0 py-2 gap-2 text-body font-normal text-text-subtle";
+
+type AppBarProps = ComponentProps<"nav"> & {
+  asChild: true;
 };
 
-export default function AppBar({
-  as: Comp = "nav",
-  className,
-  ...props
-}: AppBarProps) {
+export default function AppBar({ asChild, className, ...props }: AppBarProps) {
   return (
-    <Comp
+    <Slot
       className={cn(
-        "px-6 py-3 flex gap-6 items-center font-heading",
+        "px-8 py-3 flex gap-6 items-center font-heading  border-b border-border",
         className,
       )}
       {...props}
@@ -23,41 +22,21 @@ export default function AppBar({
   );
 }
 
-type ItemProps =
-  | ({
-      asChild: true;
-      children: ReactNode;
-      className?: string;
-    } & Omit<ComponentProps<"a">, "children" | "className">)
-  | ({
-      asChild?: false;
-      children: ReactNode;
-      className?: string;
-    } & LinkProps);
+type ItemProps = ComponentProps<typeof Link> & {
+  asChild?: boolean;
+};
 
-function Item(props: ItemProps) {
-  const itemClassName = cn(
-    "flex items-center text-action cursor-pointer transition select-none bg-transparent text-primary highlight hover-highlight px-0 py-2 gap-2 text-body font-normal text-text-subtle",
-    props.className,
-  );
+function Item({ asChild, className, ...props }: ItemProps) {
+  const Comp = asChild ? Slot : Link;
 
-  if (props.asChild) {
-    const { asChild: _, className: __, ...slotProps } = props;
-    return <Slot className={itemClassName} {...slotProps} />;
-  }
-
-  const { asChild: _, className: __, ...linkProps } = props;
-  return <Link className={itemClassName} {...linkProps} />;
+  return <Comp className={cn(itemClassName, className)} {...props} />;
 }
 
-function Logo(props: ItemProps) {
+function Logo({ className, ...props }: ItemProps) {
   return (
     <Item
+      className={cn("text-text font-bold text-2xl py-0 mr-auto", className)}
       {...props}
-      className={cn(
-        "flex-1 text-text font-bold text-2xl py-0 before:hidden",
-        props.className,
-      )}
     />
   );
 }
--- src/components/AppBar/NavBar.tsx
+++ src/components/AppBar/NavBar.tsx
@@ -13,17 +13,24 @@ export default function NavBar() {
       <span className="pointer-events-none absolute -bottom-px left-1/2 -translate-x-1/2 w-screen border-t border-dashed border-border" />
       <span className="pointer-events-none absolute left-0 bottom-0 translate-y-1/2 h-[200vh] border-l border-dashed border-border" />
       <span className="pointer-events-none absolute right-0 bottom-0 translate-y-1/2 h-[200vh] border-r border-dashed border-border" />
-      <AppBar>
-        <AppBar.Logo href="/#">MMDEV_STUDIO</AppBar.Logo>
-        <AppBar.Item href={`/#${sections.identity}`}>identity</AppBar.Item>
-        <AppBar.Item href={`/#${sections.products}`}>products</AppBar.Item>
-        <AppBar.Item href={`/#${sections.showcase}`}>showcase</AppBar.Item>
-        <AppBar.Item
-          href={`/#${sections.contact}`}
-          className="font-bold text-primary px-3"
-        >
-          build now
-        </AppBar.Item>
+
+      <AppBar asChild>
+        <nav>
+          <AppBar.Logo className="mr-auto" href="/#">
+            MMDEV_STUDIO
+          </AppBar.Logo>
+
+          <AppBar.Item href={`/#${sections.identity}`}>identity</AppBar.Item>
+          <AppBar.Item href={`/#${sections.products}`}>products</AppBar.Item>
+          <AppBar.Item href={`/#${sections.showcase}`}>showcase</AppBar.Item>
+
+          <AppBar.Item
+            href={`/#${sections.contact}`}
+            className="font-bold text-primary"
+          >
+            build now
+          </AppBar.Item>
+        </nav>
       </AppBar>
     </div>
   );
