815b4d1eb26bb2cc584a6baf172cc071a0257ea3
Marco Simon Zillgen <marcosimonzillgen@gmail.com>
2026-08-23 00:47:47 +0200

    fix(Appbar): responsive (TODO: remove Appbar and create fully indepent bars)

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -20,29 +20,41 @@ export default function Appbar({
   const [isOpen, setIsOpen] = useState(false);
   const Tag = isNav ? "nav" : "footer";
   return (
-    <Tag className="px-6 py-3 flex justify-between items-center">
+    <Tag
+      className={cn(
+        "px-6 py-3 flex justify-between items-center lg:flex-row gap-4 lg:gap-0",
+        isNav ? "" : "flex-col",
+      )}
+    >
       {/* Title */}
-      <h1 className="text-text font-bold text-2xl font-heading">
+      <h1
+        className={cn(
+          "text-text font-bold text-2xl font-heading",
+          isNav ? "" : "w-full lg:w-fit",
+        )}
+      >
         {isNav ? <Link href="/">{title}</Link> : title}
       </h1>
 
       {/* Mobile Toggle Button */}
-      <Button
-        onClick={() => setIsOpen((prev) => !prev)}
-        aria-expanded={isOpen}
-        aria-label="Toggle navigation menu"
-        className="text-text lg:hidden p-2"
-        mode="ghost"
-      >
-        {isOpen ? (
-          <Cross1Icon height={32} width={32} />
-        ) : (
-          <HamburgerMenuIcon height={32} width={32} />
-        )}
-      </Button>
+      {isNav && (
+        <Button
+          onClick={() => setIsOpen((prev) => !prev)}
+          aria-expanded={isOpen}
+          aria-label="Toggle navigation menu"
+          className={"text-text lg:hidden p-2"}
+          mode="ghost"
+        >
+          {isOpen ? (
+            <Cross1Icon height={32} width={32} />
+          ) : (
+            <HamburgerMenuIcon height={32} width={32} />
+          )}
+        </Button>
+      )}
 
       {/* Mobile Dropdown Menu */}
-      {isOpen && (
+      {isNav && isOpen && (
         <div className="absolute top-full left-0 w-full bg-bg bg-stripes border-border border flex flex-col p-4 gap-6 lg:hidden">
           {navItems.map((l, i) => {
             const highlight = i === navItems.length - 1 && isNav;
@@ -68,7 +80,14 @@ export default function Appbar({
       )}
 
       {/* Desktop */}
-      <div className="hidden lg:flex gap-6">
+      <div
+        className={cn(
+          "lg:gap-6",
+          isNav
+            ? "hidden lg:flex"
+            : "flex flex-wrap flex-col lg:flex-row items-start lg:items-center lg:w-fit w-full",
+        )}
+      >
         {navItems.map((l, i) => {
           const highlight = i === navItems.length - 1 && isNav;
 
