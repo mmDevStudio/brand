9c6e397f5518a8d49f1c566d7bea294230a36f87
Marco Simon Zillgen <marcosimonzillgen@gmail.com>
2026-08-23 00:18:27 +0200

    refactor: (home)/_components now have their own dir if needed

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,7 @@
 import Contact from "./_components/Contact";
 import Hero from "./_components/Hero";
-import Identity from "./_components/Identity";
-import Products from "./_components/Products";
+import Identity from "./_components/Identity/Identity";
+import Products from "./_components/Products/Products";
 import Showcase from "./_components/Showcase";
 
 export default function Home() {
